163befc89ba85eba7f18127b52c9cd6420953e54
wismo-s <stepha2128.af@gmail.com>
2024-01-22 19:39:34 -0500

    update setings

--- dar_comerce/settings.py
+++ dar_comerce/settings.py
@@ -87,9 +87,14 @@ WSGI_APPLICATION = 'dar_comerce.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default':  dj_database_url.config(default='postgresql://postgresql@localhost/postgres', conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
+database_url = os.environ.get("DATABASES_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
